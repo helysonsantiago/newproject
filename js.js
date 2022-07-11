72e418f51c973f4a22e4319b5b12e466319f4d92
helysonsantiago <helysonsantiago2000@gmail.com>
2022-07-11 14:17:52 -0300

    atualizaçao

--- js.js
+++ js.js
@@ -7,4 +7,21 @@ mobile.addEventListener('click',() => {
     menu.classList.toggle('active')
 
     
+    
 })
+
+const p = document.querySelector("#section");
+
+const position = p.getBoundingClientRect();
+
+    if (position.y < 0){
+
+        let nav = document.querySelector("#nav");
+        nav.style.background= "#0d1117";
+        
+    }else if(position.y == 0){
+
+        let nav = document.querySelector("#nav");
+        nav.style.background= "#161b22";
+
+    }
